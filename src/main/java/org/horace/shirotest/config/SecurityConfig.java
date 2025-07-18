f5347731829587c208778e522ab894f8afe6ab47
horace <horce1024@gmail.com>
2025-07-18 17:34:35 +0800

    增加部分配置

--- src/main/java/org/horace/shirotest/config/SecurityConfig.java
+++ src/main/java/org/horace/shirotest/config/SecurityConfig.java
@@ -1,8 +1,38 @@
 package org.horace.shirotest.config;
 
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.spring.security.interceptor.AuthorizationAttributeSourceAdvisor;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfig {
     //todo 对相关配置进行配置和注入
+
+    /**
+     * 开启shiro注解支持
+     *
+     * @param securityManager 安全管理
+     * @return
+     */
+    @Bean
+    public AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor(SecurityManager securityManager) {
+        AuthorizationAttributeSourceAdvisor authorizationAttributeSourceAdvisor = new AuthorizationAttributeSourceAdvisor();
+        authorizationAttributeSourceAdvisor.setSecurityManager(securityManager);
+        return authorizationAttributeSourceAdvisor;
+    }
+
+    /**
+     * 安全管理实体
+     *
+     * @param platformRealm 平台的认证规则
+     * @return
+     */
+    @Bean
+    public SecurityManager securityManager(PlatformRealm platformRealm) {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(platformRealm);
+        return securityManager;
+    }
 }
